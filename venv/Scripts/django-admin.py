b25722cc3e3e8a38b136cd090ccf641e7a830446
Vibiy <Vibiy@yandex.ru>
2018-06-14 12:38:07 +0300

    1

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\research.msk\PycharmProjects\untitledWEB_APP\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
